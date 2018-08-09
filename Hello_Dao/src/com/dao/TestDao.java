75a9625cc58ba345479d8094e4c5bab6c5b2b056
dz <1920098158@qq.com>
2018-08-09 20:13:21 +0800

    模块项目

---
+++ Hello_Dao/src/com/dao/TestDao.java
@@ -0,0 +1,7 @@
+package com.dao;
+
+public class TestDao {
+    public static void getDao(){
+        System.out.println("这是DAO");
+    }
+}
---
+++ Hello_Entity/src/com/entity/TestEntity.java
@@ -0,0 +1,7 @@
+package com.entity;
+
+public class TestEntity {
+    public static void getEntity(){
+        System.out.println("这是实体模块");
+    }
+}
